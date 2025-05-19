3c5ebac466ce8d1a14ccff453bba5dc16d516008
Carl Henrique <carlhenriquedev@gmail.com>
2025-05-19 01:43:19 -0300

    first

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $hostname = "localhost";
-    $usuario = "root";
-    $password = "";
-    $bancodedados = "clientes";
-
-    $conexao = new mysqli($hostname, $usuario, $password, $bancodedados);
-?>
